2f2fefc9c7deaf3bc131b23a20006e47f2574905
lxy20161001 <32894704+lxy20161001@users.noreply.github.com>
2018-08-27 13:25:46 +0800

    Update LinearSystem.h

--- LinearSystem.h
+++ LinearSystem.h
@@ -13,80 +13,123 @@
 
 using namespace std;
 
-template <typename T>
-class LinearSystem{
+template<typename T>
+class LinearSystem {
 public:
+    T EPSILON = 1e-8;
     MinMatrix<T> Ab;
     int _m;
     int _n;
-    LinearSystem(MinMatrix<T> A,MinVector<T> b){
+    vector<int> pivots;
+
+public:
+    LinearSystem(MinMatrix<T> A, MinVector<T> b) {
         assert(A.size() == b.len());
 
-         _m = A.row_num();
-         _n = A.col_num();
+        _m = A.row_num();
+        _n = A.col_num();
 
-        //assert(_m == _n);
 
-        for(int i = 0 ; i < A.row_num();++i){
+        for (int i = 0; i < A.row_num(); ++i) {
             Ab.addMinVector(A.row_vector(i).AddElem(b.getitem(i)));
         }
 
+        pivots = {};
+
     }
 
-    LinearSystem(MinMatrix<T> *A,MinVector<T> *b){
+    LinearSystem(MinMatrix<T> *A, MinVector<T> *b) {
         assert(A->size() == b->len());
 
         _m = A->row_num();
         _n = A->col_num();
+        
 
-        assert(_m == _n);
-
-        for(int i = 0 ; i < A->row_num();++i){
+        for (int i = 0; i < A->row_num(); ++i) {
             Ab.addMinVector(A->row_vector(i).AddElem(b->getitem(i)));
         }
 
+        pivots = {};
+
     }
 
-    MinMatrix<T> _forward(){
-        int n = this->_m;
-        this->Ab = this->Ab.sorted();
-        for( int i = 0 ; i < n; ++i){
-            this->Ab = this->Ab.sorted();
-            this->Ab.elimination(i,this->Ab[i]/this->Ab[i][i]);
-
-            for( int j = i+1;j<n;++j){
-                this->Ab.elimination(j,this->Ab[j].sub(this->Ab[i]*this->Ab[j][i]));
+    T _max_row(int index_i, int index_j, int n) {
+        T best = this->Ab[index_i][index_j];
+        T ret = index_i;
+        for (int i = index_i + 1; i < n; ++i) {
+            if (this->Ab[i][index_j] > best) {
+                best = this->Ab[i][index_j];
+                ret = i;
             }
+
+        }
+        return ret;
+    }
+    
+    MinMatrix<T> _forward() {
+        int i = 0;
+        int k = 0;
+        int n = this->_m;
+
+        while (i < this->_m && k < this->_n) {
+            auto max_row = this->_max_row(i, k, this->_m);
+            this->Ab.swap(i,max_row,this->Ab[max_row],this->Ab[i]);
+
+
+            if (is_zero(this->Ab[i][k])) {
+                k = k + 1;
+            } else {
+                this->Ab.elimination(i, this->Ab[i] / this->Ab[i][k]);
+                for (int j = i + 1; j < n; ++j) {
+                    this->Ab.elimination(j, this->Ab[j].sub(this->Ab[i] * this->Ab[j][k]));
+                }
+                this->pivots.push_back(k);
+                i = i + 1;
+            }
+
         }
 
         return this->Ab;
 
     }
 
-    MinMatrix<T> _backward(){
-        int n = this->_m;
-        for(int i = n-1;i>-1;--i){
-            for(int j = i - 1;j>-1;--j){
-                this->Ab.elimination(j,this->Ab[j].sub(this->Ab[i]*this->Ab[j][i]));
+    MinMatrix<T> _backward() {
+        int n = this->pivots.size();
+        for (int i = n - 1; i > -1; --i) {
+            //主元在AB[I][K]的位置
+            T k = this->pivots[i];
+            for (int j = i - 1; j > -1; --j) {
+                this->Ab.elimination(j, this->Ab[j].sub(this->Ab[i] * this->Ab[j][k]));
             }
         }
         return this->Ab;
     }
 
-    void Gj_elem(){
+    bool Gj_elem() {
         this->_forward();
         this->_backward();
+
+        for(int i = this->pivots.size()-1;i < this->_m; ++i){
+            if(!this->is_zero(this->Ab[i][Ab[i].len()-1]))
+                return false;
+        }
+        return true;
     }
 
-    void fancy_print(){
-        for(int i = 0;i < this->_m;++i){
-            for(int j = 0;j < this->_n;++j){
-                cout<<this->Ab[i][j]<<"    ";
+    void fancy_print() {
+        for (int i = 0; i < this->_m; ++i) {
+            for (int j = 0; j < this->_n; ++j) {
+                cout << this->Ab[i][j] << "    ";
             }
-            cout<<"|"<<this->Ab[i][this->_n]<<endl;
+            cout << "|" << this->Ab[i][this->_n] << endl;
         }
     }
 
-};
+    bool is_zero(T x) {
+        return abs(x) < EPSILON;
+    }
 
-#endif //ML_CPP_LINEARSYSTEM_H
+    bool is_equal(T a, T b) {
+        return abs(a - b) < EPSILON;
+    }
+};
