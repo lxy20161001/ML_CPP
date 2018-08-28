43c30d4d6195dfb01e4a1e462cd946bbbe4c8055
lxy20161001 <32894704+lxy20161001@users.noreply.github.com>
2018-08-28 16:45:48 +0800

    Update Main_Knn.cpp

--- Main_Knn.cpp
+++ Main_Knn.cpp
@@ -8,55 +8,29 @@
 using namespace std;
 
 int main() {
-    vector<vector<double>> raw_data_X = {{3.393533211, 2.331273381},
-                                         {3.110073483, 1.781539638},
-                                         {1.343808831, 3.368360954},
-                                         {3.582294042, 4.679179110},
-                                         {2.280362439, 2.866990263},
-                                         {7.423436942, 4.696522875},
-                                         {5.745051997, 3.533989803},
-                                         {9.172168622, 2.511101045},
-                                         {7.792783481, 3.424088941},
-                                         {7.939820817, 0.791637231}};
-    vector<double> raw_data_y = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+    dataSet datasets;
 
-    MinMatrix<double> minMatrix(raw_data_X);
-    MinVector<double> minVector(raw_data_y);
+    auto x = MinMatrix<double>(datasets.load_iris_data());
+    auto y = MinVector<double>(datasets.load_iris_target());
 
-    vector<double> te = {8.093607318, 3.365731514};
-    MinVector<double> x(te);
+    ML_Ang<double > ml_ang(3);
+    auto Mix = ml_ang.train_test_split(x,y,0.2);
+    auto X_train = Mix[0];
+    auto X_test = Mix[1];
+    auto Y_train = Mix[2][0];
+    auto Y_test = Mix[2][1];
 
-    Numpy_Cpp<double> np(minMatrix, minVector);
+    auto fit = ml_ang.fit(X_train,Y_train);
 
-    vector<double> distances(static_cast<unsigned int>(minMatrix.size()));
+    cout<<fit<<endl;
 
-    for (int i = 0; i < minMatrix.size(); ++i) {
-        distances[i] = sqrt(np.sum((minMatrix[i] - x) ^ 2));
-    }
+    auto predict_y = fit.predict(X_test);
 
-    MinVector<double> d(distances);
-
-    cout << d << endl;
-    cout << d.sorted() << endl;
-    auto nearest = np.argSort(d);
-    int k = 6;
-
-    RBtree<double, double> rBtree;
-    double ret = 0.0;
-    for (int i = 0; i < k; ++i) {
-        ret = minVector[nearest[i]];
-        if (rBtree.contains(ret)) {
-            rBtree.set(ret, rBtree.get(ret) + 1);
-        } else {
-            rBtree.add(ret, 1);
-        }
-    }
-
-    rBtree.toString();
-    cout<<endl;
-    MinMatrix<double> counter(rBtree.Counter());
-    cout<<counter[1][0]<<endl;
-
-
+    cout<<predict_y<<endl;
+    cout<<Y_test<<endl;
+    
+    auto t = fit.Test_Res(predict_y,Y_test);
+    cout<<t<<endl;
+   
     return 0;
 }
