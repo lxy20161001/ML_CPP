c138a2eacb1ca66821cdef107bf597d50b55c04e
lxy20161001 <32894704+lxy20161001@users.noreply.github.com>
2018-08-29 16:47:10 +0800

    Update Main_Knn.cpp

--- Main_Knn.cpp
+++ Main_Knn.cpp
@@ -10,27 +10,12 @@ using namespace std;
 int main() {
     dataSet datasets;
 
-    auto x = MinMatrix<double>(datasets.load_iris_data());
-    auto y = MinVector<double>(datasets.load_iris_target());
+    auto x = MinMatrix<double>(datasets.load_digits_data());
+    auto y = MinVector<double>(datasets.load_digits_target());
+
 
     ML_Ang<double > ml_ang(3);
-    auto Mix = ml_ang.train_test_split(x,y,0.2);
-    auto X_train = Mix[0];
-    auto X_test = Mix[1];
-    auto Y_train = Mix[2][0];
-    auto Y_test = Mix[2][1];
-
-    auto fit = ml_ang.fit(X_train,Y_train);
-
-    cout<<fit<<endl;
-
-    auto predict_y = fit.predict(X_test);
-
-    cout<<predict_y<<endl;
-    cout<<Y_test<<endl;
-    
-    auto t = fit.Test_Res(predict_y,Y_test);
-    cout<<t<<endl;
+    cout<<ml_ang.Socore(x,y)<<endl;
    
     return 0;
 }
