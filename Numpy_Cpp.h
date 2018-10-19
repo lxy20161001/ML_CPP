c09112b72a2e237f5906c9779b42f0500ce875ad
lxy20161001 <32894704+lxy20161001@users.noreply.github.com>
2018-10-19 13:28:03 +0800

    Update Numpy_Cpp.h

--- Numpy_Cpp.h
+++ Numpy_Cpp.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <random>
 #include "../LinReg/LinearSystem.h"
 
 using namespace std;
@@ -79,11 +80,21 @@ public:
         return ret;
     }
 
-    T mean(MinVector<T> vec){
+    T sum(MinVector<T> &&vec){
+        T ret = 0.0;
+        auto size = vec._size();
+        for(int i = 0; i < size; ++i){
+            ret += vec[i];
+        }
+
+        return ret;
+    }
+
+    T mean(MinVector<T> &vec){
         return sum(vec)/vec._size();
     }
 
-    T var(MinVector<T> vec){
+    T var(MinVector<T> &vec){
         auto x = mean(vec);
         auto ret = 0.0;
         auto size = vec._size();
@@ -97,7 +108,45 @@ public:
         return sqrt(var(vec));
     }
 
+    MinVector<T> line_space_rand(T &&st,T &&en,T &&size){
+        std::mt19937 rng;
+        rng.seed(std::random_device()());
+        std::uniform_real_distribution<double > dist(st,en);
+        vector<T> vec(size);
+        for( int i = 0; i < size;++i){
+            vec[i] = dist(rng);
+        }
+        return MinVector<T>(vec);
+    }
 
+    MinVector<T> random_(T &&num){
+        srand(static_cast<unsigned int>(time(NULL)));
+        vector<T> vec(num);
+        for(int i = 0;i<num;++i){
+            vec[i] = (rand()%100/(double)101);
+        }
+
+        return MinVector<T>(vec);
+    }
+
+    MinMatrix<T> hstack(MinMatrix<T> &&a,MinMatrix<T> &b){
+        MinMatrix<T> h_stack;
+        for( int i = 0; i < a.col_num();++i){
+            h_stack.addVector(a.col_vector(i));
+        }
+
+        for( int i = 0; i < b.col_num();++i){
+            h_stack.addVector(b.col_vector(i));
+        }
+
+        return h_stack._T();
+    }
+
+    MinMatrix<T> vec_to_mat(MinVector<T> &vec){
+        MinMatrix<T> matrix1;
+        matrix1.addVector(vec);
+        return matrix1._T();
+    }
 };
 
 #endif //NEW_ML_CPP_NUMPY_CPP_H
